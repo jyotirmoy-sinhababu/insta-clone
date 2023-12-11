ecc85ee001f5647500653fe7cf8946039878765a
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-12-11 15:07:29 +0530

    homePage and authPage created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css';
 
+import { Routes, Route } from 'react-router-dom';
+
+import HomePage from './pages/HomePage/HomePage';
+import AuthPage from './pages/AuthPage/AuthPage';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/auth' element={<AuthPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
-import { ChakraProvider } from '@chakra-ui/react';
-import { extendTheme } from '@chakra-ui/react';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { mode } from '@chakra-ui/theme-tools';
+
+import { BrowserRouter } from 'react-router-dom';
+
+const styles = {
+  global: (props) => ({
+    body: {
+      bg: mode('gray.100', '#000')(props),
+      color: mode('gray.800', 'whiteAlpha.900')(props),
+    },
+  }),
+};
 
 const config = {
   initialColorMode: 'dark',
@@ -12,13 +23,15 @@ const config = {
 };
 
 // 3. extend the theme
-const theme = extendTheme({ config });
+const theme = extendTheme({ config, styles });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ChakraProvider>
-      {' '}
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider theme={theme}>
+        {' '}
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
