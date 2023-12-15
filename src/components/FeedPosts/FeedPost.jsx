99ee8154a6d9dcbb5ed9428f82d0717bc4bcf268
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-12-15 14:45:35 +0530

    feed post created

--- src/components/FeedPosts/FeedPost.jsx
+++ src/components/FeedPosts/FeedPost.jsx
@@ -1,5 +1,18 @@
+import { Box, Image } from '@chakra-ui/react';
+import PostHeader from './PostHeader';
+import PostFooter from './PostFooter';
+
 const FeedPost = () => {
-  return <div>FeedPost</div>;
+  return (
+    <>
+      <PostHeader>
+        <Box>
+          <Image src='/img1' alt=' user profile pic' />
+        </Box>
+      </PostHeader>
+      <PostFooter />
+    </>
+  );
 };
 
 export default FeedPost;
