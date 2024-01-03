0c0244d7bf4af1f21a73de418a0d815f45b9a0b6
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-01-03 16:25:25 +0530

    navbar created

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,14 +1,17 @@
 import { Box, Flex } from '@chakra-ui/react';
 import SideBar from '../../components/SideBar/SideBar';
 import { useLocation } from 'react-router-dom';
-
 import { useSelector } from 'react-redux';
+
+import NavBar from '../../components/navbar/NavBar';
 
 const PageLayout = ({ children }) => {
   const { pathname } = useLocation();
   const authState = useSelector((state) => state.auth.user);
 
   const canRenderSidebar = pathname !== '/auth' && authState;
+  const canRenderNavbar = !authState && pathname !== '/auth';
+
   return (
     <Flex>
       {canRenderSidebar ? (
@@ -16,6 +19,9 @@ const PageLayout = ({ children }) => {
           <SideBar />
         </Box>
       ) : null}
+
+      {canRenderNavbar ? <NavBar /> : null}
+
       <Box flex={1} w={{ base: 'calc(100%-70px)', md: 'calc(100%-240px)' }}>
         {children}
       </Box>
