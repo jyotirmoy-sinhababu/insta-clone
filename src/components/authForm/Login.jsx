b3cd128a018405a592852740a3cbe850368cd36a
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-01-04 11:55:18 +0530

    worked on login comp

--- src/components/authForm/Login.jsx
+++ src/components/authForm/Login.jsx
@@ -1,10 +1,20 @@
 import { Input, Button } from '@chakra-ui/react';
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from '@chakra-ui/react';
 
 import { useState } from 'react';
+
+import uselogin from '../../hooks/uselogin';
 
 const Login = () => {
   const [inputs, setInputs] = useState({ email: '', password: '' });
   const [showPassword, setShowPassword] = useState(false);
+
+  const { loading, error, login } = uselogin();
 
   return (
     <>
@@ -28,7 +38,22 @@ const Login = () => {
           setInputs({ ...inputs, email: e.target.password });
         }}
       />
-      <Button w={'full'} colorScheme='blue' size={'sm'} fontSize={14}>
+      {error && (
+        <Alert status='error' fontSize={13} p={2} borderRadius={4}>
+          <AlertIcon fontSize={12} />
+          {error.message}
+        </Alert>
+      )}
+      <Button
+        w={'full'}
+        colorScheme='blue'
+        size={'sm'}
+        fontSize={14}
+        isLoading={loading}
+        onClick={() => {
+          login(inputs);
+        }}
+      >
         Log in
       </Button>
     </>
--- src/hooks/uselogin.js
+++ src/hooks/uselogin.js
@@ -31,7 +31,7 @@ const uselogin = () => {
     }
   };
 
-  return;
+  return { loading, error, login };
 };
 
 export default uselogin;
