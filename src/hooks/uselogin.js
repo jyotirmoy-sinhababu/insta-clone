20c4a80b7756b0b5be228f249f43545a7412f6aa
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-01-04 11:47:50 +0530

    useLogin hook

--- src/hooks/uselogin.js
+++ src/hooks/uselogin.js
@@ -1,11 +1,36 @@
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import useShowToast from './useShowToast';
-import { auth } from '../firebase/firebase';
+import { auth, firestore } from '../firebase/Firebase';
+import { doc, getDoc } from 'firebase/firestore';
+
+import { login } from '../slice/AuthstateSlice';
+import { useDispatch } from 'react-redux';
 
 const uselogin = () => {
   const showToast = useShowToast();
   const [signInWithEmailAndPassword, loading, error] =
     useSignInWithEmailAndPassword(auth);
+
+  const loginUser = async (inputs) => {
+    if (inputs.email || inputs.password) {
+      return showToast('Error', 'Please fill all field', 'error');
+    }
+    try {
+      const userCred = await signInWithEmailAndPassword(
+        inputs.email,
+        inputs.password
+      );
+      if (userCred) {
+        const docRef = doc(firestore, 'users', userCred.user.uid);
+        const docSnap = await getDoc(docRef);
+        localStorage.setItem('user-info', JSON.stringify(docSnap.data()));
+        useDispatch(login(docSnap.data()));
+      }
+    } catch (error) {
+      showToast('Error', error.message, 'error');
+    }
+  };
+
   return;
 };
 
