61d7cd43d470d909f80c09b3a7dfa7bab94f68e4
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-01-16 00:36:04 +0530

    searchIcon updated

--- src/components/SideBar/SearchIcon.jsx
+++ src/components/SideBar/SearchIcon.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
-import { Box, Flex, Tooltip } from '@chakra-ui/react';
 import { SearchLogo } from '../../assets/Constants';
 
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  Tooltip,
+  useDisclosure,
+} from '@chakra-ui/react';
+import useSearchUser from '../../hooks/useSearchUser';
+import { useRef } from 'react';
+
 const SearchIcon = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const searchRef = useRef(null);
+  const { user, isLoading, getUserProfile, setUser } = useSearchUser();
+
+  const handleSearchUser = (e) => {
+    e.preventDefault();
+    getUserProfile(searchRef.current.value);
+  };
+
   return (
     <>
       <Tooltip
@@ -21,11 +47,40 @@ const SearchIcon = () => {
           p={2}
           w={{ base: 10, md: 'full' }}
           justifyContent={{ base: 'center', md: 'flex-start' }}
+          onClick={onOpen}
         >
           <SearchLogo />
           <Box display={{ base: 'none', md: 'block' }}>Search</Box>
         </Flex>
       </Tooltip>
+
+      <Modal isOpen={isOpen} onClose={onClose} motionPreset='slideInLeft'>
+        <ModalOverlay />
+        <ModalContent bg={'black'} border={'1px solid gray'} maxW={'400px'}>
+          <ModalHeader>Search user</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <form onSubmit={handleSearchUser}>
+              <FormControl>
+                <FormLabel>Username</FormLabel>
+                <Input placeholder='asaprogrammer' ref={searchRef} />
+              </FormControl>
+
+              <Flex w={'full'} justifyContent={'flex-end'}>
+                <Button
+                  type='submit'
+                  ml={'auto'}
+                  size={'sm'}
+                  my={4}
+                  isLoading={isLoading}
+                >
+                  Search
+                </Button>
+              </Flex>
+            </form>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </>
   );
 };
