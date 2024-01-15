b0ea685b1be635ac2c39f8dfcae89a3a42fae040
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-01-15 15:59:54 +0530

    NotificationIcon created

--- src/components/SideBar/SidebarItems.jsx
+++ src/components/SideBar/SidebarItems.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import HomeLink from './HomeLink';
 import ProfileLink from './ProfileLink';
+import NotifictionIcon from './NotifictionIcon';
 
 const SidebarItems = () => {
   return (
     <>
       <HomeLink />
+      <NotifictionIcon />
       <ProfileLink />
     </>
   );
