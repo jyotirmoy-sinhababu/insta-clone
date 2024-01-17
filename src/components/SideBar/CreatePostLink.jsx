6a267932df2bdcc46706531143517bfcb4fbd1c6
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-01-17 17:57:26 +0530

    createPostLink updated

--- src/components/SideBar/CreatePostLink.jsx
+++ src/components/SideBar/CreatePostLink.jsx
@@ -1,7 +1,25 @@
-import { Box, Flex, Tooltip } from '@chakra-ui/react';
+import {
+  Box,
+  Flex,
+  Tooltip,
+  Textarea,
+  Input,
+  Button,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalFooter,
+  ModalBody,
+} from '@chakra-ui/react';
 import { CreatePostLogo } from '../../assets/Constants';
 
+import { BsFillImageFill } from 'react-icons/bs';
+
 const CreatePostLink = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <>
       <Tooltip
@@ -20,11 +38,38 @@ const CreatePostLink = () => {
           p={2}
           w={{ base: 10, md: 'full' }}
           justifyContent={{ base: 'center', md: 'flex-start' }}
+          onClick={onOpen}
         >
           <CreatePostLogo />
           <Box display={{ base: 'none', md: 'block' }}>Create</Box>
         </Flex>
       </Tooltip>
+      <Modal isOpen={isOpen} onClose={onClose} size='xl'>
+        <ModalOverlay />
+
+        <ModalContent bg={'black'} border={'1px solid gray'}>
+          <ModalHeader>Create Post</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <Textarea placeholder='Post caption...' />
+
+            <Input type='file' hidden />
+
+            <BsFillImageFill
+              style={{
+                marginTop: '15px',
+                marginLeft: '5px',
+                cursor: 'pointer',
+              }}
+              size={16}
+            />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button mr={3}>Post</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>{' '}
     </>
   );
 };
--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -16,29 +16,42 @@ const SuggestedUsers = () => {
   return (
     <VStack py={8} px={6} gap={4}>
       <SuggestedHeader />
-      <Flex alignItems={'center'} justifyContent={'space-between'} w={'full'}>
-        <Text fontSize={12} fontWeight={'bold'} color={'gray.500'}>
-          Suggested for you
-        </Text>
-        <Text
-          fontSize={12}
-          fontWeight={'bold'}
-          _hover={{ color: 'grey.400' }}
-          cursor={'pointer'}
-        >
-          Sell All
-        </Text>
-      </Flex>
+      {suggestedUser.length !== 0 && (
+        <Flex alignItems={'center'} justifyContent={'space-between'} w={'full'}>
+          <Text fontSize={12} fontWeight={'bold'} color={'gray.500'}>
+            Suggested for you
+          </Text>
+          <Text
+            fontSize={12}
+            fontWeight={'bold'}
+            _hover={{ color: 'gray.400' }}
+            cursor={'pointer'}
+          >
+            See All
+          </Text>
+        </Flex>
+      )}
       {suggestedUser?.map((user) => {
         return <SuggestedUser user={user} key={user.id} />;
       })}
-      <Box fontSize={12} color={'grey.500'} mt={5} alignSelf={'start'}>
+      <Box
+        fontSize={12}
+        color={'grey.500'}
+        mt={5}
+        alignSelf={'start'}
+        display={'flex'}
+        alignItems={'center'}
+        gap={'2'}
+      >
         2024 Built By
         <Link
           href='https://www.linkedin.com/in/jyotirmoy-sinhababu-64b9a7212?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app'
           target='_blank'
           color='blue.500'
           fontSize={14}
+          display={'flex'}
+          alignItems={'center'}
+          gap={'2'}
         >
           <FaLinkedin />
           Jyotirmoy Sinhababu
