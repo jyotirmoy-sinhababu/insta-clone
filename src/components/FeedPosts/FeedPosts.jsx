ffb4f578135dea10339f9ac31d62c44fd111b90f
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2024-01-26 16:54:18 +0530

    feed posts updated

--- src/components/FeedPosts/FeedPosts.jsx
+++ src/components/FeedPosts/FeedPosts.jsx
@@ -1,13 +1,25 @@
-import { Container } from '@chakra-ui/react';
+import { Container, Skeleton, SkeletonCircle, VStack } from '@chakra-ui/react';
+
+import useGetFeedPost from '../../hooks/useGetFeedPost';
+
 import FeedPost from './FeedPost';
 
 const FeedPosts = () => {
+  const { isLoading, posts } = useGetFeedPost();
   return (
     <Container maxW={'container.sm'} py={10} px={2}>
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
-      <FeedPost />
+      {isLoading &&
+        [1, 2, 3].map((_, index) => {
+          <VStack key={index} gap={4} alignItems={'flex-start'} mb={10}>
+            <Flex gap='2'>
+              <SkeletonCircle size='10' />
+              <VStack gap={2} alignItems={'flex-start'}>
+                <Skeleton height='10px' w={'200px'} />
+                <Skeleton height='10px' w={'200px'} />
+              </VStack>
+            </Flex>
+          </VStack>;
+        })}
     </Container>
   );
 };
--- src/hooks/useGetFeedPost.js
+++ src/hooks/useGetFeedPost.js
@@ -3,6 +3,8 @@ import { useSelector, useDispatch } from 'react-redux';
 import { setPost } from '../slice/PostSlice';
 import { userPresent } from '../slice/UserProfileSlice';
 import useShowToast from './useShowToast';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { firestore } from '../firebase/Firebase';
 
 const useGetFeedPost = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -14,14 +16,32 @@ const useGetFeedPost = () => {
   useEffect(() => {
     const getFeedPosts = async () => {
       setIsLoading(true);
+      if (authUser.following.length === 0) {
+        setIsLoading(false);
+        dispatch(setPost([]));
+        return;
+      }
+      const q = query(
+        collection(firestore, 'posts'),
+        where('createdBy', 'in', authUser.following)
+      );
       try {
+        const querySnapshot = await getDocs(q);
+        const feedPosts = [];
+        querySnapshot.forEach((doc) => {
+          feedPosts.push({ id: doc.id, ...doc.data() });
+        });
+        feedPosts.sort((a, b) => b.createdAt - a.createdAt);
+        dispatch(setPost(feedPosts));
       } catch (error) {
         showToast('Error', error.message, 'error');
       } finally {
         setIsLoading(false);
       }
     };
-  }, []);
+    if (authUser) getFeedPosts();
+  }, [authUser, showToast, setPosts, setUserProfile]);
+  return { isLoading, posts };
 };
 
 export default useGetFeedPost;
